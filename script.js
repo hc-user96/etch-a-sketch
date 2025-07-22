1c9792e5f182a102000fc4da59fc4d366021406a
hendrik <hen.chew96@googlemail.com>
2025-07-22 22:59:23 +0200

    Add commentary

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let squareSize = 16;
 let container = document.querySelector("#container");
 
 createSquaredGrid();
 
+// Colors hovered over squares in different color
 let squares = document.querySelectorAll(".squares");
 squares.forEach(square => {
     square.addEventListener("mouseout", (e) => {
@@ -9,12 +11,14 @@ squares.forEach(square => {
     })
 })
 
+// Let's user reset the grid with desired ammount of squares
+
 function createSquaredGrid() {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < squareSize; i++) {
         let innerContainer = document.createElement("div");
         container.appendChild(innerContainer);
 
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < squareSize; j++) {
             let square = document.createElement("div");
             square.classList.add("squares");
             innerContainer.appendChild(square);
