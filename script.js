e0afcfe5d528e4ca0553aca805976012fce71c47
hendrik <hen.chew96@googlemail.com>
2025-07-22 22:38:43 +0200

    Put logic to create squared grid inside a function

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let container = document.querySelector("#container");
 
-// Set up 16x16 grid of squares
-for (let i = 0; i < 16; i++) {
-    let innerContainer = document.createElement("div");
-    container.appendChild(innerContainer);
+createSquaredGrid();
 
-    for (let j = 0; j < 16; j++) {
-        let square = document.createElement("div");
-        square.classList.add("squares");
-        innerContainer.appendChild(square);
+let squares = document.querySelectorAll(".squares");
+squares.forEach(square => {
+    square.addEventListener("")
+})
+
+function createSquaredGrid() {
+    for (let i = 0; i < 16; i++) {
+        let innerContainer = document.createElement("div");
+        container.appendChild(innerContainer);
+
+        for (let j = 0; j < 16; j++) {
+            let square = document.createElement("div");
+            square.classList.add("squares");
+            innerContainer.appendChild(square);
+        }
     }
 }
-
