8c54ffc6612cc4114deee8bf0735dbcc2f7052cb
hendrik <hen.chew96@googlemail.com>
2025-07-25 17:17:34 +0200

    Add function to create squares and css to evenly size them

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+let container = document.querySelector("#container")
+
+
 // createSquareGrid(16x16)
+createSquareGrid(2);
 // eventListener("hover", changeColor())
+
 // eventlistener("click", resetGrid())
 
+
 // FUNCTION resetGrid()
+
 // FUNCTION changeColor()
+
 // FUNCTION createSquareGrid(size) 
+function createSquareGrid(size) {
+    for (let i = 0; i < size; i++) {
+        let iContainer = document.createElement("div");
+        iContainer.classList.add("inner-container");
+        container.appendChild(iContainer);
+
+        for (let j = 0; j < size; j++) {
+            let square = document.createElement("div");
+            square.classList.add("squares");
+            iContainer.appendChild(square);
+        }
+    }
+}
