03ced7d71ade300038a4501d3a0d67eb9cd4156e
hendrik <hen.chew96@googlemail.com>
2025-07-25 16:26:54 +0200

    Rework the entire js
    Earlier attempt did'nt properly work, starting with commnents now

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-let squareSize = 16;
-let container = document.querySelector("#container");
+// createSquareGrid(16x16)
+// eventListener("hover", changeColor())
+// eventlistener("click", resetGrid())
 
-createSquaredGrid();
-
-// Colors hovered over squares in different color
-let squares = document.querySelectorAll(".squares");
-squares.forEach(square => {
-    square.addEventListener("mouseout", (e) => {
-        square.classList.add("hover");
-    })
-})
-
-// Let's user reset the grid with desired ammount of squares
-
-function createSquaredGrid() {
-    for (let i = 0; i < squareSize; i++) {
-        let innerContainer = document.createElement("div");
-        container.appendChild(innerContainer);
-
-        for (let j = 0; j < squareSize; j++) {
-            let square = document.createElement("div");
-            square.classList.add("squares");
-            innerContainer.appendChild(square);
-        }
-    }
-}
+// FUNCTION resetGrid()
+// FUNCTION changeColor()
+// FUNCTION createSquareGrid(size) 
